bde339a89e553f5a1c507d9b4cac234b26662f02
freeze0 <kras2001@bk.ru>
2022-10-23 16:40:26 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+
+def bisections (func, bounds, eps = 1e-4):
+    a, b = bounds
+    if func(a) * func(b) > 0:
+        print('На заданном интервале нет корней!')
+        return
+    x0 = (a + b) / 2
+    while abs(a - b) >= eps:
+        if func(a) * func(x0) > 0:
+            a = x0
+        else:
+            b = x0
+        x0 = (a + b) / 2
+    return x0
+
+def func(x):
+    return math.exp(x) - 6 * x - 3
+
+x = bisections(func, (-1, 3))
+print('Полученный х = ', x, '\n', 'Значение функции в найденой точке: ', func(x), '\n')
