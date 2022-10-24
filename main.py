a0f19cef8420fbd837c425c96d9b11b39cf40718
freeze0 <44439196+freeze0@users.noreply.github.com>
2022-10-24 12:19:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import math
 
-def bisections (func, bounds, eps = 1e-4):
+def biss(func, bounds, eps=1e-5):
     a, b = bounds
     if func(a) * func(b) > 0:
-        print('На заданном интервале нет корней!')
-        return
+        print('На заданном интервале нет корней')
+        exit(0)
     x0 = (a + b) / 2
     while abs(a - b) >= eps:
         if func(a) * func(x0) > 0:
@@ -14,9 +14,10 @@ def bisections (func, bounds, eps = 1e-4):
         x0 = (a + b) / 2
     return x0
 
-def func(x):
-    return math.exp(x) - 6 * x - 3
 
-x = bisections(func, (-1, 3))
+def func(x):
+    return x ** 2  # math.sin(x), x**2-3, math.exp(x) - 6 * x - 3
+
+x = biss(func, (-1, 3))
+
 print('Полученный х = ', x, '\n', 'Значение функции в найденой точке: ', func(x), '\n')
-print(x)
