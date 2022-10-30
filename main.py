0b41029785bcb93d3f2297db98c46a4b3cf98777
freeze0 <kras2001@bk.ru>
2022-10-30 19:59:49 +0300

    Commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import math
+from scipy.misc import derivative
 
 def biss(func, bounds, eps=1e-5):
     a, b = bounds
     if func(a) * func(b) > 0:
-        print('На заданном интервале нет корней')
-        exit(0)
+        x1 = -3
+        if (proizv(0) == 0):
+            print('Полученный х = ', proizv(0), '\n', 'Значение функции в найденой точке: ', proizv(0), '\n')
+            exit(0)
+        elif (func(x1) == 0):
+            print('Полученный х = ', x1, '\n', 'Значение функции в найденой точке: ', func(x1), '\n')
+            exit(0)
+        else:
+            print('На заданном интервале нет корней')
+            exit(0)
     x0 = (a + b) / 2
     while abs(a - b) >= eps:
         if func(a) * func(x0) > 0:
@@ -16,8 +25,10 @@ def biss(func, bounds, eps=1e-5):
 
 
 def func(x):
-    return x ** 2  # math.sin(x), x**2-3, math.exp(x) - 6 * x - 3
+    return x ** 2  # math.sin(x) (-1, 3), -x ** 2 + 4 (-2, 5), math.exp(x) - 6 * x - 3 (-6, 3), (x + 3) ** 2, x ** 2
+def proizv(x):
+    return derivative(func, x)
 
-x = biss(func, (-1, 3))
+x = biss(func, (-6, 3))
 
 print('Полученный х = ', x, '\n', 'Значение функции в найденой точке: ', func(x), '\n')
