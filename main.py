ff634f6c30b633c8fcc0030ea2938f85c09f7183
freeze0 <44439196+freeze0@users.noreply.github.com>
2022-10-31 12:23:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import math
-from scipy.misc import derivative
+import sys
 
 def biss(func, bounds, eps=1e-5):
     a, b = bounds
     if func(a) * func(b) > 0:
-        x1 = -3
-        if (proizv(0) == 0):
-            print('Полученный х = ', proizv(0), '\n', 'Значение функции в найденой точке: ', proizv(0), '\n')
+        x0 = biss(proizv, bounds)
+        if (math.isnan(x0)):
+            print('Корней нет на интервале')
             exit(0)
-        elif (func(x1) == 0):
-            print('Полученный х = ', x1, '\n', 'Значение функции в найденой точке: ', func(x1), '\n')
-            exit(0)
-        else:
-            print('На заданном интервале нет корней')
-            exit(0)
+        return x0
     x0 = (a + b) / 2
     while abs(a - b) >= eps:
         if func(a) * func(x0) > 0:
@@ -25,10 +20,12 @@ def biss(func, bounds, eps=1e-5):
 
 
 def func(x):
-    return x ** 2  # math.sin(x) (-1, 3), -x ** 2 + 4 (-2, 5), math.exp(x) - 6 * x - 3 (-6, 3), (x + 3) ** 2, x ** 2
-def proizv(x):
-    return derivative(func, x)
+    return math.sin(x)+math.cos(x)
+#math.sin(x**2) (-4.2, 12) math.sin(x)+math.cos(x) (-5, 6) ln(x+2) x**2 (-4.2, 12) (x-3)**2 (-1, 3)
 
-x = biss(func, (-6, 3))
+def proizv(x):
+    return math.cos(x)-math.sin(x)
+
+x = biss(func, (-5, 6))
 
 print('Полученный х = ', x, '\n', 'Значение функции в найденой точке: ', func(x), '\n')
